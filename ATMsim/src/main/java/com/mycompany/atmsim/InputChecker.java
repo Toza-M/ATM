ea0b75e65cc7a2ddee8e918ebc9d8154f013e654
omarhelalx <omarhelal951@gmail.com>
2025-05-15 14:41:23 +0300

    fixed

--- ATMsim/src/main/java/com/mycompany/atmsim/InputChecker.java
+++ ATMsim/src/main/java/com/mycompany/atmsim/InputChecker.java
@@ -18,17 +18,7 @@ public class InputChecker {
      * Email,Name,Password
      */
 
-    public static boolean isValidUser(String cardNumber, String pin) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("admin_users.txt"))) {
 
-        functions to check the correct PIN & Card number for users 
-        and to check the correct Email and Password for admins
-        You need to make the files to test this funcs
-        the template for users.txt:
-            Name,Card Number,PIN,Balance
-        the template for admins.txt:
-            Email,Name,Password
-    */
     
      public static boolean isValidUser(String cardNumber, String pin) {
         try (BufferedReader reader = new BufferedReader(new FileReader("users.txt"))) {
